bbb6e1c7e15cddc54a752effc475a5a8c40c5137
diego <diego.vasquez.l@uni.pe>
2022-05-28 20:42:49 -0500

    [IMP] some variables and methods have been renamed

--- src/cowboycheckers/usuarios/Jugador.java
+++ src/cowboycheckers/usuarios/Jugador.java
@@ -7,30 +7,30 @@ import java.util.ArrayList;
 import cowboycheckers.modelo.JugarPieza;
 
 public class Jugador {
-	private String name;
+	private String nombre;
 	private Color color;
-	private ArrayList<JugarPieza> pieces;
-	final Integer MAXPIECES = 9;
+	private ArrayList<JugarPieza> piezas;
+	final Integer MAXPIEZAS = 9;
 
-	public Jugador(String name, String color){
-		this.name = name;
+	public Jugador(String nombre, String color){
+		this.nombre = nombre;
 		this.setColor(color);
-		this.initPieces();
+		this.initPiezas();
 	}
 
-	private void initPieces() {
-		this.pieces = new ArrayList<JugarPieza>(MAXPIECES);
-		for(Integer i=0; i<MAXPIECES; i++){
+	private void initPiezas() {
+		this.piezas = new ArrayList<JugarPieza>(MAXPIEZAS);
+		for(Integer i=0; i<MAXPIEZAS; i++){
 			JugarPieza p = new JugarPieza(this.color, this, i);
-			this.pieces.add(p);
+			this.piezas.add(p);
 		}
 	}
 
-	public JugarPieza getPiece(int id)
+	public JugarPieza getPieza(int id)
 	{
-		for(int i=0; i < pieces.size(); i++)
-			if (pieces.get(i).getID() == id)
-				return pieces.get(i);
+		for(int i=0; i < piezas.size(); i++)
+			if (piezas.get(i).getID() == id)
+				return piezas.get(i);
 
 		return null;
 	}
@@ -47,40 +47,40 @@ public class Jugador {
 		this.color = c;
 	}
 
-	public String getName() {
-		return this.name;
+	public String getNombre() {
+		return this.nombre;
 	}
 
 	public Color getColor() {
 		return this.color;
 	}
 
-	public ArrayList<JugarPieza> getPieces() {
-		return this.pieces;
+	public ArrayList<JugarPieza> getPiezas() {
+		return this.piezas;
 	}
 
-	public Integer getScore() {
+	public Integer getPuntuacion() {
 		int curscore = 0;
 
-		for(int i=0; i < pieces.size(); i++)
-			if (pieces.get(i).getEstado() != JugarPieza.MUERTO)
+		for(int i=0; i < piezas.size(); i++)
+			if (piezas.get(i).getEstado() != JugarPieza.MUERTO)
 				curscore++;
 
 		return curscore;
 	}
 
-	public int getPiecesPlayed() {
+	public int getPiezasJugadas() {
 		int played = 0;
-		for (int i=0; i < pieces.size(); i++)
+		for (int i=0; i < piezas.size(); i++)
 		{
-			if (pieces.get(i).getEstado() != JugarPieza.NOPOSICIONADO)
+			if (piezas.get(i).getEstado() != JugarPieza.NOPOSICIONADO)
 				played++;
 		}
 
 		return played;
 	}
 
-	public boolean isHuman() {
+	public boolean esHumano() {
 		return true;
 	}
 }
