f10c1b0170ca94b7f2ef2ad2ddb5fa66697cd0f2
Joel Jimenez <Jjimenezch.uni.pe>
2022-05-29 17:13:05 -0500

    Iniciando con la clase VentanaInicio

---
+++ src/cowboycheckers/vistas/VentanaInicio.java
@@ -0,0 +1,14 @@
+package cowboycheckers.vistas;
+//Creando la ventana de inicio para en la interfaz de usuario
+
+import javax.swing.*;
+
+public class VentanaInicio extends JComponent {
+    private static final long serialVersionUID = 5219597153026471980L;
+    private VentanaPrincipal mw;
+    private JButton pve;
+    private JPanel top;
+    private JPanel bot;
+    private JButton pvp;
+    private JLabel welcome;
+}
