3b3629dba2fe02a9d14cd3982a195425bf8dc8e3
Joel Jimenez <Jjimenezch.uni.pe>
2022-05-29 17:26:00 -0500

    Creamos carpetas para mantener un orden al momento de crear clases

--- src/cowboycheckers/vistas/VentanaInicio.java
+++ src/cowboycheckers/vistas/VentanaInicio.java
@@ -5,10 +5,17 @@ import javax.swing.*;
 
 public class VentanaInicio extends JComponent {
     private static final long serialVersionUID = 5219597153026471980L;
+    private JLabel welcome;
     private VentanaPrincipal mw;
     private JButton pve;
-    private JPanel top;
     private JPanel bot;
     private JButton pvp;
-    private JLabel welcome;
+    private JPanel top;
+
+    public VentanaInicio(VentanaPrincipal mw) {
+
+
+
+
+    }
 }
